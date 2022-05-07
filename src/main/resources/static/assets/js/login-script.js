15902983aad042a8d746c917ef46d8f6bcd0a239
Helena Xu <hxu1617@students.cs.ubc.ca>
2022-05-07 16:49:59 -0700

    Added basic user-auth for login page

--- src/main/resources/static/assets/js/login-script.js
+++ src/main/resources/static/assets/js/login-script.js
@@ -1,31 +1,44 @@
 console.log("lol wut");
 
-const signInForm = document.getElementById("sign_in");
-const signUpForm = document.getElementById("sign_up");
+const signInForm = document.getElementById("sign-in");
+const signUpForm = document.getElementById("sign-up");
 
 const passwordPane = document.getElementById("password");
 const submitButton = document.getElementById("login")
 
-const display_name_btn = document.createElement("input");
-display_name_btn.setAttribute("type", "text");
-display_name_btn.setAttribute("id", "displayName");
-display_name_btn.setAttribute("name", "display name");
-display_name_btn.setAttribute("placeholder", "display name");
+const displayName = document.createElement("input");
+displayName.setAttribute("type", "text");
+displayName.setAttribute("id", "displayName");
+displayName.setAttribute("name", "display name");
+displayName.setAttribute("placeholder", "display name");
 
-// <input type="text" id="displayName" class="fadeIn fourth" name="display name" placeholder="display name">
+const loginForm = document.getElementById("login-form")
 
 signInForm.addEventListener("click", function() {
     signInForm.setAttribute("class", "active");
     signUpForm.setAttribute("class", "inactive underlineHover");
-    display_name_btn.remove();
+    displayName.remove();
     submitButton.setAttribute("value", "Log In");
 });
 
 signUpForm.addEventListener("click", function() {
      signUpForm.setAttribute("class", "active");
      signInForm.setAttribute("class", "inactive underlineHover");
-     passwordPane.parentNode.insertBefore(display_name_btn, passwordPane.nextSibling);
+     passwordPane.parentNode.insertBefore(displayName, passwordPane.nextSibling);
       submitButton.setAttribute("value", "Register");
+});
+
+submitButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    if (email == "user" && password == "web_dev") {
+        alert("You have successfully logged in.");
+        window.location.href = "/inner-page.html";
+    } else {
+        alert("Incorrect credentials");
+    }
 });
 
 //fun register() {
@@ -36,7 +49,7 @@ signUpForm.addEventListener("click", function() {
 //        },
 //        body: JSON.stringify({ email: "demo@example.com", password: "password", displayName: "demo" })
 //    })
-}
+//}
 
 
 
